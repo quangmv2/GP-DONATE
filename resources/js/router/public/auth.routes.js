43d39ac76af941180626edcc28353643c890f3cd
Mai Van Quang <maiquang1470@gmail.com>
2020-06-29 15:31:48 +0700

    commit cho suwj choi ngu

---
+++ resources/js/router/public/auth.routes.js
@@ -0,0 +1,15 @@
+
+import Login from "components/PublicPages/Login/Login";
+
+export const authRoutes = [
+    {
+        path: "/",
+        component: Login,
+        exact: true
+    },
+    {
+        path: "/client/login",
+        component: Login,
+        exact: true
+    }
+];
