e3ea31b037da365afc8888f8b8471ba6dbc2e148
Hoang Bui <hoang.bui@saigontechnology.com>
2020-07-14 09:36:32 +0700

    update fix conflict

--- resources/js/components/PrivatePages/index.js
+++ resources/js/components/PrivatePages/index.js
@@ -1,38 +1,30 @@
-import Dashboard from "./Dashboard";
-import PostOffer from "./PostOffer/PostOffer";
-import HomePage from "./Home/HomePage";
-<<<<<<< HEAD
+import Dashboard from './Dashboard';
+import PostOffer from './PostOffer/PostOffer';
+import HomePage from './Home/HomePage';
+
+import ProfileSetting from './ProfileSetting';
+import EditProfile from './EditProfile';
 import PostComment from './HomeComment/PostComment';
-import UserProfile from './UserProfile/UserProfile';
-import MessagesDetail from './Activities/MessageDetail/MessagesDetail';
+import Search from './Search';
+import PostLike from './PostLike';
 import ActivitiesScreen from './Activities/ActivitiesScreen/ActivitiesScreen';
 import MessagesComponent from './Activities/MessagesComponent/MessagesComponent';
 import NotificationComponent from './Activities/NotificationComponent/NotificationComponent';
-export { Dashboard, PostOffer, HomePage, PostComment, UserProfile, ActivitiesScreen, MessagesComponent, 
-    NotificationComponent, MessagesDetail };
-=======
-import ProfileSetting from "./ProfileSetting";
-import EditProfile from "./EditProfile";
-import PostComment from "./HomeComment/PostComment";
-import Search from "./Search";
-import PostLike from "./PostLike";
-import ActivitiesScreen from "./Activities/ActivitiesScreen/ActivitiesScreen";
-import MessagesComponent from "./Activities/MessagesComponent/MessagesComponent";
-import NotificationComponent from "./Activities/NotificationComponent/NotificationComponent";
-import UserProfile from "./UserProfile/UserProfile";
+import UserProfile from './UserProfile/UserProfile';
+import MessagesDetail from './Activities/MessageDetail/MessagesDetail';
 
 export {
-    Dashboard,
-    PostOffer,
-    HomePage,
-    ProfileSetting,
-    EditProfile,
-    PostComment,
-    Search,
-    ActivitiesScreen,
-    MessagesComponent,
-    NotificationComponent,
-    UserProfile,
-    PostLike
+  Dashboard,
+  PostOffer,
+  HomePage,
+  ProfileSetting,
+  EditProfile,
+  PostComment,
+  Search,
+  ActivitiesScreen,
+  MessagesComponent,
+  NotificationComponent,
+  UserProfile,
+  PostLike,
+  MessagesDetail,
 };
->>>>>>> 490611e3110f3daa4645622fd6b3a34c64c531a1
--- resources/js/router/private/single.routes.js
+++ resources/js/router/private/single.routes.js
@@ -1,68 +1,64 @@
-import EditProfile from "components/PrivatePages/EditProfile/EditProfile";
-import ProfileSetting from "components/PrivatePages/ProfileSetting/ProfileSetting";
-<<<<<<< HEAD
-import { PostOffer, HomePage, PostComment, UserProfile, ActivitiesScreen, MessagesDetail } from "components/PrivatePages";
-=======
+import EditProfile from 'components/PrivatePages/EditProfile/EditProfile';
+import ProfileSetting from 'components/PrivatePages/ProfileSetting/ProfileSetting';
 import {
-    PostOffer,
-    HomePage,
-    PostComment,
-    UserProfile,
-    ActivitiesScreen,
-    Search,
-    PostLike
-} from "components/PrivatePages";
->>>>>>> 490611e3110f3daa4645622fd6b3a34c64c531a1
-import { PRIVATE_ROUTE } from "constants";
+  PostOffer,
+  HomePage,
+  PostComment,
+  UserProfile,
+  ActivitiesScreen,
+  Search,
+  PostLike,
+  MessagesDetail,
+} from 'components/PrivatePages';
+import { PRIVATE_ROUTE } from 'constants';
 
 export const singleRoutes = [
-    {
-        path: PRIVATE_ROUTE.HOME,
-        component: HomePage,
-        exact: true
-    },
-    {
-        path: PRIVATE_ROUTE.POST_OFFER,
-        component: PostOffer,
-        exact: true
-    },
-    {
-        path: "/profile-setting",
-        component: ProfileSetting,
-        exact: true
-    },
-    {
-        path: "/edit-profile",
-        component: EditProfile
-    },
-    {
-        path: PRIVATE_ROUTE.POST_COMMENT,
-        component: PostComment,
-        exact: true
-    },
-    {
-        path: "/search",
-        component: Search,
-        exact: true
-    },
-    {
-        path: PRIVATE_ROUTE.USER_PROFILE,
-        component: UserProfile,
-        exact: true
-    },
-    {
-        path: PRIVATE_ROUTE.ACTIVITIES,
-        component: ActivitiesScreen,
-        exact: true
-    },
-    {
-<<<<<<< HEAD
-        path: PRIVATE_ROUTE.MESSAGES,
-        component: MessagesDetail,
-=======
-        path: "/post-like",
-        component: PostLike,
->>>>>>> 490611e3110f3daa4645622fd6b3a34c64c531a1
-        exact: true
-    }
+  {
+    path: PRIVATE_ROUTE.HOME,
+    component: HomePage,
+    exact: true,
+  },
+  {
+    path: PRIVATE_ROUTE.POST_OFFER,
+    component: PostOffer,
+    exact: true,
+  },
+  {
+    path: '/profile-setting',
+    component: ProfileSetting,
+    exact: true,
+  },
+  {
+    path: '/edit-profile',
+    component: EditProfile,
+  },
+  {
+    path: PRIVATE_ROUTE.POST_COMMENT,
+    component: PostComment,
+    exact: true,
+  },
+  {
+    path: '/search',
+    component: Search,
+    exact: true,
+  },
+  {
+    path: PRIVATE_ROUTE.USER_PROFILE,
+    component: UserProfile,
+    exact: true,
+  },
+  {
+    path: PRIVATE_ROUTE.ACTIVITIES,
+    component: ActivitiesScreen,
+    exact: true,
+  },
+  {
+    path: PRIVATE_ROUTE.MESSAGES,
+    component: MessagesDetail,
+  },
+  {
+    path: '/post-like',
+    component: PostLike,
+    exact: true,
+  },
 ];
