0dc7573dc64cc4af9edddc7b36a325dac5b9cacc
Hoang Bui <hoang.bui@saigontechnology.com>
2020-07-25 12:57:33 +0700

    update fix add title and favicon

--- resources/views/client/app.blade.php
+++ resources/views/client/app.blade.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon"/>
     <link href="https://fonts.googleapis.com/css2?family=Muli:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <title>Laravel</title>
+    <title>Generous People</title>
     <!-- <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css"> -->
 </head>
 <body>
