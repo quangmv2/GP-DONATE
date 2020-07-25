ebb8b452025ccba6609d91aa5e77aaa075768256
quynhnganav@gmail.com <quynhnganav@gmail.com>
2020-07-25 20:22:13 +0700

    .

--- resources/js/components/PrivatePages/EditProfile/EditProfile.js
+++ resources/js/components/PrivatePages/EditProfile/EditProfile.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { postLogin } from "modules/auth/actions";
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import { fetchService } from "../../../services/fetch/fetchService";
+import { ROOT_API_URL, GET_IMAGE, ACCESS_TOKEN, POST_POST, ROUTE } from "../../../constants";
 import {
-    selectIsLogged,
-    selectErrors,
-    selectLoading,
+
     selectUserInfo
 } from "modules/auth/selectors";
 import { HeaderNavigation } from "components/Atoms";
@@ -19,187 +20,193 @@ import {
     LoadingOutlined,
     PlusOutlined
 } from "@ant-design/icons";
-import { GET_IMAGE } from "../../../constants/routes";
+import { openNotification } from "helpers";
+import { NOTIFICATION_TYPE } from "constants";
 
-function beforeUpload(file) {
-    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-    if (!isJpgOrPng) {
-        message.error("You can only upload JPG/PNG file!");
+
+const EditProfile = (props) => {
+    const { userInfo } = props;
+    const [name, setName] = useState('');
+    const [foudation, setFoudation] = useState('');
+    const [image, setImage] = useState(userInfo.personal_photo);
+    const [fullPhoto, setFullPhoto] = useState(userInfo.full_photo);
+    const submit = async () => {
+        event.preventDefault();
+        let data = {
+            first_name: name,
+            username: userInfo.username,
+            personal_photo: image,
+            full_photo: fullPhoto,
+            foudation: foudation,
+        }
+        const [res, status] = await fetchService.fetch(`${ROOT_API_URL}/api/profile/me`, {
+            method: "PUT",
+            body: JSON.stringify(data)
+        });
+        if (status == 200) {
+            openNotification(NOTIFICATION_TYPE.SUCCESS, 'Success');
+            const { history } = props;
+            history.push(ROUTE.MYPROFILE)
+           
+        } else if (status == 422) {
+            const keys = Object.keys(res.errors);
+            keys.forEach(key => openNotification(NOTIFICATION_TYPE.ERROR, res.errors[key]));
+
+        } else {
+            openNotification(NOTIFICATION_TYPE.ERROR, 'Error', '')
+           
+        }
     }
-    const isLt2M = file.size / 1024 / 1024 < 4;
-    if (!isLt2M) {
-        message.error("Image must smaller than 4MB!");
+
+
+
+
+    const uploadFullphoto = async (e) => {
+        var formData = new FormData();
+        formData.append("photo", e.target.files[0]);
+        const res = await fetchService.upload(`${ROOT_API_URL}/api/photo/up`, false, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+                "Accept": "application/json",
+                'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }).then(data => {
+            const { image_directory } = JSON.parse(data);
+            if (image_directory) {
+                setFullPhoto(image_directory);
+                console.log(fullPhoto);
+            }
+        });
     }
-    return isJpgOrPng && isLt2M;
-}
-
-export class EditProfile extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            description: "",
-            offervalue: "",
-            duedate: "",
-            hastag: "",
-            errorValidLogin: {}
-        };
-        this.setSubmitting = null;
+    const uploadAvatar = async (e) => {
+        var formData = new FormData();
+        formData.append("photo", e.target.files[0]);
+        const res = await fetchService.upload(`${ROOT_API_URL}/api/photo/up`, false, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+                "Accept": "application/json",
+                'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }).then(data => {
+            const { image_directory } = JSON.parse(data);
+            if (image_directory) {
+                setImage(image_directory)
+            }
+        });
     }
-    async componentDidMount() { 
-    }
+    const inputName = (e) => {
+        setName(e.target.value);
+    };
+    const changeFoudation = (e) => {
+        setFoudation(e.target.value);
+        console.log(e.target.value)
+    };
 
-    componentDidUpdate(prevProps) { }
 
-    render() {
-        const { errors, loading, userInfo } = this.props;
 
-        const uploadButton = (
-            <div>
-                {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
-                <div className="ant-upload-text">Upload</div>
-            </div>
-        );
-        return (
-            <div className="private-fullheight">
-                <div className="container">
-                    <HeaderNavigation headerName="Edit Profile" />
+    return (
+        <div className="private-fullheight">
+            <div className="container">
+                <HeaderNavigation headerName="Edit Profile" />
 
-                    <div className="bgImg">
-                        <div className="info-bg">
+                <div className="bgImg">
+                    <div className="info-bg">
 
-                           {userInfo.code_id !== null ? <div className="image">
-                                <StarFilled className="icon-star" />
-                                <img
-                                 src="/images/10.jpg"
-                                    //  src={GET_IMAGE(userInfo.personal_photo)}
-                                    alt="Girl in a jacket"
-                                    width="60"
-                                    height="60"
-                                ></img>
-                            </div> : <div className="image"><img
-                                    src="/images/10.jpg"
-                                     //  src={GET_IMAGE(userInfo.personal_photo)}
-                                    alt="Girl in a jacket"
-                                    width="60"
-                                    height="60"
-                                ></img> </div>}
-                            <span className="text">Change your avatar</span>
-                        </div>
-                    </div>
-                    <div className="body-wrapper wrapper-profile">
-                        <Formik
-                            initialValues={{
-                                name: "",
-                                username: "",
-                                foundation: ""
-                            }}
-                            validate={values => {
-                                const errors = {};
-                                if (!values.username) {
-                                    errors.username = "Required";
-                                }
+                        {userInfo.code_id !== null ? <div className="image">
+                            <StarFilled className="icon-star" />
+                            <img
+                                src={GET_IMAGE(image)}
+                                alt="Girl in a jacket"
+                                width="60"
+                                height="60"
+                            ></img>
+                        </div> : <div className="image"><img
+                            src={GET_IMAGE(image)}
+                            //  src={GET_IMAGE(userInfo.personal_photo)}
+                            alt="Girl in a jacket"
+                            width="60"
+                            height="60"
+                        ></img> </div>}
+                        <label htmlFor="upload"> <span className="text">Change your avatar</span></label>
+                        <input id="upload" type="file" name="photo" style={{ visibility: 'hidden' }} onChange={uploadAvatar} />
 
-                                if (!values.name) {
-                                    errors.name = "Required";
-                                }
-
-                                return errors;
-                            }}
-                            onSubmit={(values, actions) => {
-                                console.log(values);
-                            }}
-                        >
-                            {({
-                                values,
-                                errors,
-                                touched,
-                                handleChange,
-                                //handleBlur,
-                                handleSubmit,
-                                isSubmitting
-                                /* and other goodies */
-                            }) => (
-                                    <form onSubmit={handleSubmit}>
-                                        <div className="form-submit">
-                                            <TextField
-                                                className="form-text"
-                                                id="standard-password-input"
-                                                label="Name"
-                                                type="text"
-                                                required
-                                                name="name"
-                                                value={values.name}
-                                                onChange={handleChange}
-                                                error={errors.name && touched.name}
-                                                helperText={
-                                                    touched.name ? errors.name : ""
-                                                }
-                                            />
-                                        </div>
-                                        <div className="form-submit">
-                                            <label className='username-label'>User name</label>
-                                            <TextField
-                                                disabled
-                                                className="form-text"
-                                                id="standard-password-input"
-                                                placeholder= {userInfo.username}
-                                                type="text"
-                                                name="username"
-                                                value={values.username}
-                                                onChange={handleChange}
-                                                error={
-                                                    errors.username &&
-                                                    touched.username
-                                                }
-                                                helperText={
-                                                    touched.username
-                                                        ? errors.username
-                                                        : ""
-                                                }
-                                            />
-                                        </div>
-                                        <div className="form-submit">
-                                            <TextField
-                                                className="form-text"
-                                                id="standard-password-input"
-                                                label="Foudation"
-                                                type="text"
-                                                name="foundation"
-                                                onChange={handleChange}
-                                                value={values.foundation}
-                                            />
-                                        </div>
-
-                                        <div className="form-submit form-upload">
-                                            <Upload
-                                                name="avatar"
-                                                listType="picture-card"
-                                                className="avatar-uploader"
-                                                showUploadList={false}
-                                                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                                                beforeUpload={beforeUpload}
-                                                onChange={this.handleChange}
-                                            >
-                                                {uploadButton}
-                                            </Upload>
-                                        </div>
-                                        <button
-                                            type="submit"
-                                            className="button"
-                                            onClick={handleSubmit}
-                                        >
-                                            Submit
-                                    </button>
-                                    </form>
-                                )}
-                        </Formik>
                     </div>
                 </div>
+                <div className="body-wrapper wrapper-profile">
+                    <form >
+                        <div className="form-submit">
+                            <TextField
+                                className="form-text"
+                                id="standard-password-input"
+                                label="Name"
+                                type="text"
+                                required
+                                name="name"
+                                value={name}
+                                onChange={inputName}
+
+                            />
+                        </div>
+                        <div className="form-submit">
+                            <label className='username-label'>User name</label>
+                            <TextField
+                                disabled
+                                className="form-text"
+                                id="standard-password-input"
+                                placeholder={userInfo.username}
+                                type="text"
+                                name="username"
+
+
+                            />
+                        </div>
+                        <div className="form-submit">
+                            <TextField
+                                className="form-text"
+                                id="standard-password-input"
+                                label="Foudation"
+                                type="text"
+                                name="name"
+                                value={foudation}
+                                onChange={changeFoudation}
+
+                            />
+                        </div>
+
+                       { userInfo.full_photo !== null ? <><label htmlFor="fullPhoto"> <img
+                                src={GET_IMAGE(fullPhoto)}
+                                className='full-photo-image'
+                                alt="Girl in a jacket"
+                                width="100%"
+                                height="100%"
+                            ></img></label>
+                            <input id="fullPhoto" type="file" name="fullPhoto" style={{ visibility: 'hidden' }} onChange={uploadFullphoto} /> </>
+                            : <div className="form-submit form-upload form-upload-fullPhoto">
+                            <label htmlFor="fullPhoto"><PhotoCameraIcon style={{ fontSize: '37px' }} /></label>
+                            <input id="fullPhoto" type="file" name="fullPhoto" style={{ visibility: 'hidden' }} onChange={uploadFullphoto} />
+                        </div>}
+
+                        <button
+                            type="submit"
+                            className="button edit-profile-button"
+                            onClick={submit}
+                        >
+                            Submit
+                    </button>
+                    </form>
+
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 const mapDispatchToProps = {
 };
@@ -213,9 +220,5 @@ EditProfile.defaultProps = {
     errors: {}
 };
 
-EditProfile.propTypes = {
-    login: PropTypes.func,
-    isLogged: PropTypes.bool
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditProfile);
